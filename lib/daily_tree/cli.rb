42d4fe7be9ae8720b94722e982baacb525ffe7f1
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-03-13 18:59:14 +0000

    building out empty functionality to cli

--- lib/daily_tree/cli.rb
+++ lib/daily_tree/cli.rb
@@ -2,7 +2,16 @@ class DailyTree::CLI
 
 def call
   puts "The tree of the day is:"
+  todays_tree
+  stats
+
 end
 
+  def todays_tree
+    puts "*Red Oak* - 'Desription of the tree'"
+  end
 
+  def stats
+    puts "Would you to see this tree's stats?"
+end
 end
