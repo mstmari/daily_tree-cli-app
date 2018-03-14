8963737832d4f6285fefdf612c7c19a5f6ff48ff
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-03-14 16:39:25 +0000

    Resctructuring from 'fake code' to have my program start creating objects

--- lib/daily_tree/cli.rb
+++ lib/daily_tree/cli.rb
@@ -1,34 +1,36 @@
 class DailyTree::CLI
 
-def call
-  todays_tree
+#1. I want to check the load path, make sure that I am on the right path
 
+
+def call
+  DailyTree::Tree.new #<-- When I call this I want it to create a new instance of tree
+  todays_tree #<-- this puts "The tree of the day is: and puts the #name of the tree"
   goodbye
 end
 
   def todays_tree
-    DailyTree::Tree.today
-    menu
+    Tree.name
   end
 
-    def description
-    puts "Here is a tree that is hardly ever seen and is not even widely available and yet it is one of the most adaptable and versatile trees available."
-    end
+    # def description
+    # puts "Here is a tree that is hardly ever seen and is not even widely available and yet it is one of the most adaptable and versatile trees available."
+    # end
 
-    def gets_stats
+    # def stats
+    #
+    #   puts "NAME: Western soapberry
+    #
+    #   LATIN: Sapindus drummondii
+    #
+    #   HEIGHT: 30 feet
+    #
+    #   TYPE:  Deciduous"
+    # end
 
-      puts "NAME: Western soapberry
-
-      LATIN: Sapindus drummondii
-
-      HEIGHT: 30 feet
-
-      TYPE:  Deciduous"
-    end
-
-    def goodbye
-      puts "Be sure to check back tomorrow for another tree!"
-    end
+    # def goodbye
+    #   puts "Be sure to check back tomorrow for another tree!"
+    # end
 
 
     def menu
@@ -38,7 +40,7 @@ end
          input = gets.strip.downcase
           case input
           when "stats"
-            gets_stats
+            stats
           when "description"
             description
           when "exit"
--- lib/daily_tree/tree.rb
+++ lib/daily_tree/tree.rb
@@ -1,15 +1,38 @@
 class DailyTree::Tree
   attr_accessor :name, :description, :stats
 
+  @@all = []
 
-  def self.today
-    puts "The tree of the day is:"
-    puts "*Western Soapberry*"
+  def initialize(name = nil)
+    @name = name
+    @description = description
+    @stats = stats
 
-    pupu = tree = self.new
-    tree.name = "Western soapberry"
-    tree.description = puts "tree description"
-    tree.stats = puts "tree stats"
+    @@all << self
+
+  end
+
+  #Here I want to create a new object of a tree that instantiates with a name, stats and a description.
+
+  def self.today #<-- I want to be able to call this on the new instance of tree and have it the name of the tree
+    puts "The tree of the day is: #{self.name}"
+  end
+
+  def self.description #<- I want to be able to call this method and have it give the description of my tree
+    #@description
+    puts "Here is a tree that is hardly ever seen and is not even widely available and yet it is one of the most adaptable and versatile trees available."
+  end
+
+  def self.stats #<-- I want to be able to call this and have it give the stats from the tree.
+    #@stats
+
+    puts "NAME: Western soapberry
+
+    LATIN: Sapindus drummondii
+
+    HEIGHT: 30 feet
+
+    TYPE:  Deciduous"
   end
 
 
