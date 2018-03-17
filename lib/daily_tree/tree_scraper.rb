1c84b4c6b03504e67070a37a9258f24701870f1c
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-03-17 20:21:06 +0000

    started to build tree scraping method

--- lib/daily_tree/tree_scraper.rb
+++ lib/daily_tree/tree_scraper.rb
@@ -3,23 +3,29 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-  binding.pry
+  #binding.pry
 
-def self.scrape_tree_index_page
-  tree = Nokogiri::HTML(open("https://plants.ces.ncsu.edu/plants/category/trees/"))
-  scraped_tree_names = []
+# def self.scrape_tree_index_page
+#   tree = Nokogiri::HTML(open("https://plants.ces.ncsu.edu/plants/category/trees/"))
+#   scraped_tree_names = []
+#
+#     tree.css("#plant-table-div").each do |i|
+#       tree_names = {common_name: i.css(".td-comm-name").text,
+#        scientific_name: i.css(".th-sci-name a").text}
+#       scraped_tree_names.push(tree_names)
+#     end
+#     scraped_tree_names
+#   end
 
-    tree.css("#plant-table-div").each do |i|
-      tree_names = {common_name: i.css(".td-comm-name").text,
-       scientific_name: i.css(".th-sci-name a").text}
-      scraped_tree_names.push(tree_names)
-    end
-    scraped_tree_names
-  end
-  tree.css(".td-comm-name a").text each do |i|
+
+binding.pry
+  def self.scrape_tree_data
+    tree_data = {}
+    doc = Nokogiri::HTML(open("https://plants.ces.ncsu.edu/plants/trees/asimina-triloba-sunflower/"))
+    doc.css("dl").each do |i|
+       #tree_data [:comment] = doc.css(".value value-comment").text
 
 end
-
 
 
 
