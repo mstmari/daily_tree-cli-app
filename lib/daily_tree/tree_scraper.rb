b601eb26e9a1a54760faaf60ea8e1285e8e729b3
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-03-18 17:16:13 +0000

    added clarity to comments

--- lib/daily_tree/tree_scraper.rb
+++ lib/daily_tree/tree_scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-
+binding.pry
 def self.scrape_tree_index_page
   tree = Nokogiri::HTML(open("https://plants.ces.ncsu.edu/plants/category/trees/"))
   scraped_tree_names = []
@@ -20,18 +20,19 @@ def self.scrape_tree_index_page
 
   def self.scrape_tree_data
     tree_data = {}
-    #This part of the method scrapes the individual plant page.
-    #I need to figure out how to automatically access the page instead of hardcoding it in, like below.
+    #This first part of the method scrapes the individual plant page.
+    #***I need to figure out how to automatically access the page instead of hardcoding it in, like below.***
     doc = Nokogiri::HTML(open("https://plants.ces.ncsu.edu/plants/trees/asimina-triloba-sunflower/"))
     new_array = doc.css(".plant_details").text.split("\n")
 
     #this part of the method parses the raw data found in '.plant_details' container.
-    #There was some very weird spacing and set up, I first had to remove extra '"' and ',' that were used to space out the data.
+    #There was some very weird spacing and set up, I first had to remove extra '"' and ',' and empty spaces that were originally
+    #used to space out the data on the website.
     new_array.reject!{|a| a.nil? || (a.to_s.gsub(' ', '') == '') }
-    #The next line splits the information from the 'key' and 'value' into different arrays so I can operate on it and assign it.
+    #The next line splits the information at the ":" placing the 'key' and 'value' into different arrays so I can operate on and assign them.
      plant = new_array.map{|i| i.split(":")}
 
-    #this next part iterates through the array of key and value and assigns the charecteristics I want, and ignores the ones I do not. 
+    #this next part iterates through the array of key and value and assigns the charecteristics I want, and ignores the ones I do not.
       plant.each do |i|
         key = i[0].gsub(" ","")
         value = i[1]
